6cd68e15054a768c051a7a4f6dd06fb20a3bf730
Gabriel Lavoie <lavg2007@github.com>
2020-01-18 12:48:12 -0500

    Cmake fonctionnel

---
+++ CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gabriel/Documents/ENGINE/src/app.cpp" "/home/gabriel/Documents/ENGINE/CMakeFiles/engine.dir/src/app.cpp.o"
+  "/home/gabriel/Documents/ENGINE/src/game_object.cpp" "/home/gabriel/Documents/ENGINE/CMakeFiles/engine.dir/src/game_object.cpp.o"
+  "/home/gabriel/Documents/ENGINE/src/graphics.cpp" "/home/gabriel/Documents/ENGINE/CMakeFiles/engine.dir/src/graphics.cpp.o"
+  "/home/gabriel/Documents/ENGINE/src/player.cpp" "/home/gabriel/Documents/ENGINE/CMakeFiles/engine.dir/src/player.cpp.o"
+  "/home/gabriel/Documents/ENGINE/src/shader.cpp" "/home/gabriel/Documents/ENGINE/CMakeFiles/engine.dir/src/shader.cpp.o"
+  "/home/gabriel/Documents/ENGINE/src/tile.cpp" "/home/gabriel/Documents/ENGINE/CMakeFiles/engine.dir/src/tile.cpp.o"
+  "/home/gabriel/Documents/ENGINE/src/timer.cpp" "/home/gabriel/Documents/ENGINE/CMakeFiles/engine.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/SDL2"
+  "include"
+  "glm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/src/app.cpp.o"
+  "CMakeFiles/engine.dir/src/game_object.cpp.o"
+  "CMakeFiles/engine.dir/src/graphics.cpp.o"
+  "CMakeFiles/engine.dir/src/player.cpp.o"
+  "CMakeFiles/engine.dir/src/shader.cpp.o"
+  "CMakeFiles/engine.dir/src/tile.cpp.o"
+  "CMakeFiles/engine.dir/src/timer.cpp.o"
+  "engine.pdb"
+  "engine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
